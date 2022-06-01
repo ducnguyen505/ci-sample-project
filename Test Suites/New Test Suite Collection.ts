a25001f874a21516744fe1189b4307c4f3b8ac74
Duc Nguyen <duc-tnguyen@katalon.com>
2022-06-01 23:08:48 +0700

    update

--- Test Suites/New Test Suite Collection.ts
+++ Test Suites/New Test Suite Collection.ts
@@ -3,30 +3,16 @@
    <description></description>
    <name>New Test Suite Collection</name>
    <tag></tag>
-   <delayBetweenInstances>20</delayBetweenInstances>
+   <delayBetweenInstances>15</delayBetweenInstances>
    <executionMode>PARALLEL</executionMode>
-   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <maxConcurrentInstances>3</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
-            <runConfigurationData>
-               <entry>
-                  <key>testCloudEnvironment</key>
-                  <value>{
-  &quot;id&quot;: 2,
-  &quot;os&quot;: &quot;linux&quot;,
-  &quot;osDisplayName&quot;: &quot;Linux&quot;,
-  &quot;browserName&quot;: &quot;chrome&quot;,
-  &quot;browserDisplayName&quot;: &quot;Chrome&quot;,
-  &quot;browserVersion&quot;: &quot;88&quot;,
-  &quot;usingTunnel&quot;: false,
-  &quot;timeout&quot;: 3
-}</value>
-               </entry>
-            </runConfigurationData>
-            <runConfigurationId>TestCloud</runConfigurationId>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/WebUI Keywords Test</testSuiteEntity>
@@ -35,25 +21,21 @@
          <configuration>
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
-            <runConfigurationData>
-               <entry>
-                  <key>testCloudEnvironment</key>
-                  <value>{
-  &quot;id&quot;: 4,
-  &quot;os&quot;: &quot;linux&quot;,
-  &quot;osDisplayName&quot;: &quot;Linux&quot;,
-  &quot;browserName&quot;: &quot;firefox&quot;,
-  &quot;browserDisplayName&quot;: &quot;Firefox&quot;,
-  &quot;browserVersion&quot;: &quot;86&quot;,
-  &quot;usingTunnel&quot;: false,
-  &quot;timeout&quot;: 3
-}</value>
-               </entry>
-            </runConfigurationData>
-            <runConfigurationId>TestCloud</runConfigurationId>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/WebUI Keywords Test</testSuiteEntity>
+         <testSuiteEntity>Test Suites/WebUI Keywords Test 1</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/WebUI Keywords Test 2</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
